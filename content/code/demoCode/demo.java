8025264d7279a167769647d51cff544df59cc1bb
Achilles <lytreetree@gmail.com>
2021-09-03 09:19:56 +0100

    finish the DivideAndConquerSort

--- content/code/demoCode/demo.java
+++ content/code/demoCode/demo.java
@@ -3,5 +3,7 @@ package demoCode;
 public class demo{
     public static void main(String[] args) {
         System.out.println("Hello , World!");
+        int a = (int) (1.8/2) ;
+        System.out.println(a);
     }
 }
