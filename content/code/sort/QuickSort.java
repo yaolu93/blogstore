ddc7afbe081187b1d071b2a77fa25795a9f82903
unknown <lytreetree@gmail.com>
2021-09-08 06:15:11 +0800

    Insertion Sort

--- content/code/sort/QuickSort.java
+++ content/code/sort/QuickSort.java
@@ -35,7 +35,7 @@ public class QuickSort {
 
         //security check when we are using the recursion
         if(high <= low){
-            return;
+            break;
         }
         int partition = partition(a, low, high);
         // using the partition to divide the Array into two different groups. which also
