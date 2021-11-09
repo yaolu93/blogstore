4e83f2bd3740935e3f515b6626dbc2a2df74b63f
Achilles <lytreetree@gmail.com>
2021-11-09 00:17:07 +0000

    278

---
+++ content/code/.leetcode/278.first-bad-version.java
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=278 lang=java
+ *
+ * [278] First Bad Version
+ *
+ * https://leetcode.com/problems/first-bad-version/description/
+ *
+ * algorithms
+ * Easy (39.39%)
+ * Likes:    3385
+ * Dislikes: 1190
+ * Total Accepted:    730.4K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '5\n4'
+ *
+ * You are a product manager and currently leading a team to develop a new
+ * product. Unfortunately, the latest version of your product fails the quality
+ * check. Since each version is developed based on the previous version, all
+ * the versions after a bad version are also bad.
+ * 
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the
+ * first bad one, which causes all the following ones to be bad.
+ * 
+ * You are given an API bool isBadVersion(version) which returns whether
+ * version is bad. Implement a function to find the first bad version. You
+ * should minimize the number of calls to the API.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 5, bad = 4
+ * Output: 4
+ * Explanation:
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ * Then 4 is the first bad version.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 1, bad = 1
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= bad <= n <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(isBadVersion(mid)){
+                right = mid;
+            }else{
+                left = mid +1;
+            }
+        }
+        return left;
+        
+        
+    }
+}
+// @lc code=end
+
