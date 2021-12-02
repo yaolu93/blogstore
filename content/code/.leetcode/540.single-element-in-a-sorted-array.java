33d912b78cab452cd58ec440f298baa48e7da6b2
Achilles <lytreetree@gmail.com>
2021-12-02 11:34:43 +0000

    new method for 540

--- content/code/.leetcode/540.single-element-in-a-sorted-array.java
+++ content/code/.leetcode/540.single-element-in-a-sorted-array.java
@@ -51,8 +51,33 @@ class Solution {
         //     }
         // }
         // return nums[length];
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left < right){
+            int mid = left+(right-left)/2;
+            boolean restIsEven = (right - mid)%2 == 0;
+            if(nums[mid] == nums[mid+1]){
+                if(restIsEven){
+                    left = mid + 2 ;
+                }else{
+                    right = mid - 1;
+                }
+
+            }else if(nums[mid] == nums[mid-1]){
+                if(restIsEven){
+                    right = mid -2;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                return nums[mid];
+            }
+        }
+
+        return nums[left];
         
-    }
+   } 
 }
 // @lc code=end
 
