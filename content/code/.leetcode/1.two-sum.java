7f5cedc908bc8bf35226b4387d2252310725a454
Achilles <lytreetree@gmail.com>
2022-05-08 22:17:12 +0000

    two sum again

--- content/code/.leetcode/1.two-sum.java
+++ content/code/.leetcode/1.two-sum.java
@@ -62,15 +62,17 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-     int n = nums.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
+        
+        int lengths = nums.length;
+
+        for (int i = 0; i < lengths; i++) {
+            for (int j = 1+i; j < lengths; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+                    return new int[]{i,j};
                 }
             }
         }
-        return new int[0];
+        return null;
 
     }
 }
