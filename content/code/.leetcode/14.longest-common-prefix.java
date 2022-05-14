21301f0dde90606a4587849d8130613bfe79f878
Achilles <lytreetree@gmail.com>
2022-05-14 22:42:16 +0000

    22

--- content/code/.leetcode/14.longest-common-prefix.java
+++ content/code/.leetcode/14.longest-common-prefix.java
@@ -58,7 +58,7 @@ class Solution {
 
         for (int i = 1; i < lengths; i++) {
             
-            while (prefix.indexOf(strs[i]) != 0) {
+            while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length()-1);
             }
         }
