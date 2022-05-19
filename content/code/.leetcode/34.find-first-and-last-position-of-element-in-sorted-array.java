36e89aecd656b2e703a4f04c243cebdb2ae03c6c
Achilles <lytreetree@gmail.com>
2022-05-19 22:32:55 +0000

    2

---
+++ content/code/.leetcode/34.find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=34 lang=java
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (40.08%)
+ * Likes:    10635
+ * Dislikes: 293
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.7M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in non-decreasing order, find the
+ * starting and ending position of a given target value.
+ * 
+ * If target is not found in the array, return [-1, -1].
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * Example 2:
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ * Example 3:
+ * Input: nums = [], target = 0
+ * Output: [-1,-1]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * nums is a non-decreasing array.
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+    }
+}
+// @lc code=end
+
