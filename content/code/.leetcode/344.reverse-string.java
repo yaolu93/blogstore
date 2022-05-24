1572e4e676db3e70db38a43f4221408e28bf926c
Achilles <lytreetree@gmail.com>
2022-05-24 20:59:03 +0000

    344

---
+++ content/code/.leetcode/344.reverse-string.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=344 lang=java
+ *
+ * [344] Reverse String
+ *
+ * https://leetcode.com/problems/reverse-string/description/
+ *
+ * algorithms
+ * Easy (74.92%)
+ * Likes:    5074
+ * Dislikes: 945
+ * Total Accepted:    1.6M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '["h","e","l","l","o"]'
+ *
+ * Write a function that reverses a string. The input string is given as an
+ * array of characters s.
+ * 
+ * You must do this by modifying the input array in-place with O(1) extra
+ * memory.
+ * 
+ * 
+ * Example 1:
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 10^5
+ * s[i] is a printable ascii character.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public void reverseString(char[] s) {
+        int a_pointer = 0;
+        int b_pointer = s.length - 1;
+
+        while(a_pointer <= b_pointer){
+            char temp = s[a_pointer];
+            s[a_pointer] = s[b_pointer];
+            s[b_pointer] = temp;
+            a_pointer += 1;
+            b_pointer -= 1;
+        }
+
+
+    }
+}
+// @lc code=end
+
