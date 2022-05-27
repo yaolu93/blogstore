25c951353cfce8765084611dbd843d8c99a35b21
Achilles <lytreetree@gmail.com>
2022-05-27 15:53:42 +0000

    345

---
+++ content/code/.leetcode/345.reverse-vowels-of-a-string.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=345 lang=java
+ *
+ * [345] Reverse Vowels of a String
+ *
+ * https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+ *
+ * algorithms
+ * Easy (46.98%)
+ * Likes:    1633
+ * Dislikes: 1853
+ * Total Accepted:    353.6K
+ * Total Submissions: 751.8K
+ * Testcase Example:  '"hello"'
+ *
+ * Given a string s, reverse only all the vowels in the string and return it.
+ * 
+ * The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both
+ * cases.
+ * 
+ * 
+ * Example 1:
+ * Input: s = "hello"
+ * Output: "holle"
+ * Example 2:
+ * Input: s = "leetcode"
+ * Output: "leotcede"
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 3 * 10^5
+ * s consist of printable ASCII characters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseVowels(String s) {
+        
+    }
+}
+// @lc code=end
+
