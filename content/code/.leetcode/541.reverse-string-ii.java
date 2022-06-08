21584cb5956fd7cb05075211f7a342fa79469507
Achilles <lytreetree@gmail.com>
2022-06-08 21:32:25 +0000

    2

---
+++ content/code/.leetcode/541.reverse-string-ii.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=541 lang=java
+ *
+ * [541] Reverse String II
+ *
+ * https://leetcode.com/problems/reverse-string-ii/description/
+ *
+ * algorithms
+ * Easy (50.16%)
+ * Likes:    1010
+ * Dislikes: 2457
+ * Total Accepted:    154.9K
+ * Total Submissions: 308.6K
+ * Testcase Example:  '"abcdefg"\n2'
+ *
+ * Given a string s and an integer k, reverse the first k characters for every
+ * 2k characters counting from the start of the string.
+ * 
+ * If there are fewer than k characters left, reverse all of them. If there are
+ * less than 2k but greater than or equal to k characters, then reverse the
+ * first k characters and leave the other as original.
+ * 
+ * 
+ * Example 1:
+ * Input: s = "abcdefg", k = 2
+ * Output: "bacdfeg"
+ * Example 2:
+ * Input: s = "abcd", k = 2
+ * Output: "bacd"
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 10^4
+ * s consists of only lowercase English letters.
+ * 1 <= k <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseStr(String s, int k) {
+        
+    }
+}
+// @lc code=end
+
