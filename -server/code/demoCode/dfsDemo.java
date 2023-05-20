1da38bfb3d6de3e85483efccc9d3dc435740a1f9
Achilles <lytreetree@gmail.com>
2023-05-20 16:43:31 +0100

    delete -server and -w

--- -server/code/demoCode/dfsDemo.java
+++
@@ -1,6 +0,0 @@
-package demoCode;
-public class dfsDemo {
-    public static void main(String[] args) {
-        System.out.println("Hello , World!");
-    }
-}
--- -w/code/demoCode/demo.java
+++
@@ -1,9 +0,0 @@
-package demoCode;
-
-public class demo{
-    public static void main(String[] args) {
-        System.out.println("Hello , World!");
-        int a = (int) (1.8/2) ;
-        System.out.println(a);
-    }
-}
