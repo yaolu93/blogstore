5199817b12d7dcb8e7bbf303a1d950aea6d55966
yaolu <lytreetree@gmail.com>
2023-05-27 14:34:24 +0100

    added public ignore again

--- copy_hugo_public.sh
+++ copy_hugo_public.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-# 清空目标文件夹
-rm -rf yao_blog/*
-mkdir -p yao_blog
+# 保存 .git 文件夹和 CNAME 文件
+mv /home/yaolu/fromGithub/yao_blog/.git /tmp
+mv /home/yaolu/fromGithub/yao_blog/CNAME /tmp
 
-# 复制 public 内容到目标文件夹，排除 CNAME 文件
-rsync -av --exclude 'CNAME' public/ yao_blog/
+
+# # 清空目标文件夹
+# rm -rf /home/yaolu/fromGithub/yao_blog/*
+
+
+# 清空目标文件夹下的所有文件，除了 .git 和 CNAME
+find /home/yaolu/fromGithub/yao_blog -mindepth 1 ! -name '.git' ! -name 'CNAME' -delete
+
+# 将 .git 和 CNAME 文件移回目标文件夹
+mv /tmp/.git /home/yaolu/fromGithub/yao_blog
+mv /tmp/CNAME /home/yaolu/fromGithub/yao_blog
+
+# 复制 public 内容到目标文件夹
+cp -r public/* /home/yaolu/fromGithub/yao_blog
+
