e7f6af5b01b65e00081bd80c89c9e082a6e6d169
yaolu <yao.lu@openet.com>
2024-05-06 17:01:01 +0100

    num 3, sliding window and hashset

---
+++ content/code/.leetcode/leetcode/editor/en/[3]Longest Substring Without Repeating Characters.java
@@ -0,0 +1,76 @@
+//Given a string s, find the length of the longest substring without repeating 
+//characters. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "abcabcbb"
+//Output: 3
+//Explanation: The answer is "abc", with the length of 3.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3.
+//Notice that the answer must be a substring, "pwke" is a subsequence and not a 
+//substring.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= s.length <= 5 * 10⁴ 
+// s consists of English letters, digits, symbols and spaces. 
+// 
+//
+// Related Topics Hash Table String Sliding Window 👍 39188 👎 1849
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        //1.滑動窗口
+        //2.哈希表
+        //3.雙指針
+        int n = s.length();
+        int left = 0;
+        Set<Character> charSet = new HashSet<>();
+
+        int maxLength = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (!charSet.contains(s.charAt(right))) {
+                charSet.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+            } else {
+
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+                charSet.add(s.charAt(right));
+            }
+        }
+        return maxLength;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+//4.動態規劃
