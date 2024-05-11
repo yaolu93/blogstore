19049fb2cd9624d244b57877fce9456202081fd1
YaoLu <lytreetree@gmail.com>
2024-05-11 21:32:26 +0000

    On branch master Your branch and 'origin/master' have diverged, and have 1 and 4 different commits each, respectively.
    Changes to be committed:
    	modified:   codeleet/demoCode/src/main/java/org/achilles/Main.java

--- codeleet/demoCode/src/main/java/org/achilles/Main.java
+++ codeleet/demoCode/src/main/java/org/achilles/Main.java
@@ -1,28 +1,53 @@
 package org.achilles;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        Entry[] entry = { new Entry(2, 1),
+                new Entry(3, 7),
+                new Entry(7, 3),
+                new Entry(1, 6),
+                new Entry(5, 5),
+                new Entry(3, 2),
+                new Entry(4, 4) };
+
+        int maxX = 0;
+        for (Entry sizeX : entry) {
+            maxX = Math.max(maxX, sizeX.getX());
+            System.out.printf("maxX: %d%n", maxX);
         }
+
+        int maxY = 0;
+        for (Entry sizeY : entry) {
+            maxY = Math.max(maxY, sizeY.getX());
+            System.out.printf("maxY: %d%n", maxY);
+        }
+
+        for (int i = maxX - 1; i >= 0; i--) {
+            for (int j = maxY; j > 0; j--) {
+                System.out.print("- ");
+            }
+            System.out.println();
+        }
+
     }
-    class Entry{
+
+    public static class Entry {
         private int x;
         private int y;
 
-        Entry(int x, int y){
-            this.x = x; 
+        Entry(int x, int y) {
+            this.x = x;
             this.y = y;
         }
 
         public int getX() {
             return x;
         }
+
         public int getY() {
             return y;
         }
