aafbf1596cc9198c1946360462f019b58d427529
Achilles <lytreetree@gmail.com>
2024-05-13 20:51:08 +0100

    Array/String

---
+++ content/code/.leetcode/leetcode/editor/en/[345]Reverse Vowels of a String.java
@@ -0,0 +1,34 @@
+/**
+Given a string s, reverse only all the vowels in the string and return it. 
+
+ The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower 
+and upper cases, more than once. 
+
+ 
+ Example 1: 
+ Input: s = "hello"
+Output: "holle"
+ 
+ Example 2: 
+ Input: s = "leetcode"
+Output: "leotcede"
+ 
+ 
+ Constraints: 
+
+ 
+ 1 <= s.length <= 3 * 10⁵ 
+ s consist of printable ASCII characters. 
+ 
+
+ Related Topics Two Pointers String 👍 4455 👎 2771
+
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String reverseVowels(String s) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ content/code/.leetcode/leetcode/editor/en/[605]Can Place Flowers.java
@@ -0,0 +1,39 @@
+/**
+You have a long flowerbed in which some of the plots are planted, and some are 
+not. However, flowers cannot be planted in adjacent plots. 
+
+ Given an integer array flowerbed containing 0's and 1's, where 0 means empty 
+and 1 means not empty, and an integer n, return true if n new flowers can be 
+planted in the flowerbed without violating the no-adjacent-flowers rule and false 
+otherwise. 
+
+ 
+ Example 1: 
+ Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+ 
+ Example 2: 
+ Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+ 
+ Constraints: 
+
+ 
+ 1 <= flowerbed.length <= 2 * 10⁴ 
+ flowerbed[i] is 0 or 1. 
+ There are no two adjacent flowers in flowerbed. 
+ 0 <= n <= flowerbed.length 
+ 
+
+ Related Topics Array Greedy 👍 6415 👎 1141
+
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
