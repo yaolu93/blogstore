1a0b119a058817d07b881ae05efd99d196f71ac1
Achilles <lytreetree@gmail.com>
2024-05-26 14:46:07 +0100

    1207

--- content/code/.leetcode/leetcode/editor/en/[1207]Unique Number of Occurrences.java
+++ content/code/.leetcode/leetcode/editor/en/[1207]Unique Number of Occurrences.java
@@ -1,3 +1,8 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
 Given an array of integers arr, return true if the number of occurrences of 
 each value in the array is unique or false otherwise. 
@@ -40,7 +45,34 @@ Output: true
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        
+        //api: getorDefault
+        //key:1, value:3    key:2, value:2    key:3, value:1
+        // my personal idea is using the count to comparing the hashmap's size()
+
+        int n = arr.length;
+        int count = 0;
+        Map<Integer, Integer> arrMap = new HashMap<>();
+        Set<Integer> arrSet = new HashSet<>();
+
+        for (int ele : arr){
+            arrMap.put(ele, arrMap.getOrDefault(ele, 0) + 1);
+            System.out.println(arrMap);
+        }
+
+        for (int ele : arrMap.values()){
+            arrSet.add(ele);
+            System.out.println(arrMap.keySet());
+
+        }
+
+        for (int ele : arrSet){
+            if (arrMap.containsKey(ele)){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        return arrMap.size() == arrSet.size();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
