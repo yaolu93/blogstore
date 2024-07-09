83d1d2d33197f88d8d107f0122ee2acc515c1306
Achilles <lytreetree@gmail.com>
2024-07-09 06:43:23 +0100

    fixed the ooi error in 169

--- content/code/.leetcode/189.rotate-array.java
+++ content/code/.leetcode/189.rotate-array.java
@@ -62,28 +62,20 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
-
-        if(k < 0){
-            k += nums.length;
-        }
-
-        reverse(nums, 0, nums.length - k - 1);
-        reverse(nums, nums.length - k, nums.length - 1);
         reverse(nums, 0, nums.length - 1);
+        reverse(nums, k, nums.length - 1);
+        reverse(nums, 0, k -1);
+
     }
 
+    public static void reverse(int[] nums, int left, int right){
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
 
-    public static void reverse(int nums[], int i, int j){
-        int li = i;
-        int ri = j;
-
-        while (li < ri) {
-            int temp = nums[li];
-            nums[li] = nums[ri];
-            nums[ri] = temp;
-
-            li++;
-            ri++;
+            left++;
+            right--;
         }
     }
 }
