658c48eafb969792988a323a9e6beba1eb84c495
Achilles <lytreetree@gmail.com>
2026-01-05 22:36:03 +0000

    update content

--- linux_update_hugo_blog.sh
+++ linux_update_hugo_blog.sh
@@ -3,7 +3,7 @@
 hugo
 
 git status
-git pull
+git pull --rebase
 git status
 git add .
 git status
@@ -13,22 +13,22 @@ git push
 git status
 
 # 保存 .git 文件夹和 CNAME 文件
-mv /home/yaolu/fromGithub/yao_blog/.git /tmp
-mv /home/yaolu/fromGithub/yao_blog/CNAME /tmp
+mv /home/yao/fromGithub/yao_blog/.git /tmp
+mv /home/yao/fromGithub/yao_blog/CNAME /tmp
 
 # 清空目标文件夹下的所有文件，除了 .git 和 CNAME
-find /home/yaolu/fromGithub/yao_blog -mindepth 1 ! -name '.git' ! -name 'CNAME' -delete
+find /home/yao/fromGithub/yao_blog -mindepth 1 ! -name '.git' ! -name 'CNAME' -delete
 
 # 将 .git 和 CNAME 文件移回目标文件夹
-mv /tmp/.git /home/yaolu/fromGithub/yao_blog
-mv /tmp/CNAME /home/yaolu/fromGithub/yao_blog
+mv /tmp/.git /home/yao/fromGithub/yao_blog
+mv /tmp/CNAME /home/yao/fromGithub/yao_blog
 
 # 复制 public 内容到目标文件夹
-cp -r public/* /home/yaolu/fromGithub/yao_blog
+cp -r public/* /home/yao/fromGithub/yao_blog
 
-cd /home/yaolu/fromGithub/yao_blog
+cd /home/yao/fromGithub/yao_blog
 git status
-git pull
+git pull --rebase
 git status
 git add .
 git status
