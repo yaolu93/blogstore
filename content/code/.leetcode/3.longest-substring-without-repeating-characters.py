c42e38942e47b01513456097392a80afcbcfbe8b
Yao Lu <lytreetree@gmail.com>
2026-02-14 13:52:13 +0000

    modified the condition

--- content/code/.leetcode/3.longest-substring-without-repeating-characters.py
+++ content/code/.leetcode/3.longest-substring-without-repeating-characters.py
@@ -37,7 +37,7 @@
 # Example 3:
 # 
 # 
-# Input: s = "pwwkew"
+# Input: s = "pwwkewwww"
 # Output: 3
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a
